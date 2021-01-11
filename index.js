abc5fcb02f3af0612aa8c5a96a9bae4d133a86cb
Referee1536 <66706835+Referee1536@users.noreply.github.com>
2021-01-11 18:59:26 -0500

    Corrected the code, missed a couple things

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // REMEMBER TO FILL IN THE CLIENT ID              V V V V V V V
-const client = require('discord-rich-presence')('798297420928122890');
+const client = require('discord-rich-presence')('client-id-here');
 
 client.updatePresence({
   // Fill in the text and customize it to your liking
-  details: 'Hey there!',
-  state: 'This is awesome!',
+  details: 'Secondary Text',
+  state: 'Tertiary text',
 
   // If you added any images
   // Remove this section if you don't want any images
-  largeImageKey: 'red',
-  largeImageText: 'I like red',
+  largeImageKey: 'image-name-here',
+  largeImageText: 'Text that appears when a mouse is hovered over the image',
+
+  // If you would like a small icon-like image
+  // Remove this section if you don't want this
+  smallImageKey: 'image-name-here',
+  smallImageText: 'Text that appears when a mouse is hovered over the image',
 
   // If you wish to add any links
   // Remove this if you do not want any links
   buttons: [
-    {label: 'Join my Discord Server!', url: 'https://discord.gg'},
-    {label: 'How I got my cool status!', url: 'https://github.com/Referee1536/discord-rp-tutorial'}
+    {label: 'The text on the button', url: 'The link'},
+    {label: 'The text on the button', url: 'The link'}
   ],
 
   // Do not remove
