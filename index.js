0e45d667adacdb3a91958b46b1c3795fd354cf27
Referee1536 <66706835+Referee1536@users.noreply.github.com>
2021-02-26 08:48:57 -0500

    Using double quotes instead of single

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // REMEMBER TO FILL IN THE CLIENT ID              V V V V V V V
-const client = require('discord-rich-presence')('client-id-here');
+const client = require("discord-rich-presence")("client-id-here");
 
 client.updatePresence({
   // Fill in the text and customize it to your liking
-  details: 'Secondary Text',
-  state: 'Tertiary text',
+  details: "Secondary Text",
+  state: "Tertiary text",
 
   // If you added any images
-  // Remove this section if you don't want any images
-  largeImageKey: 'image-name-here',
-  largeImageText: 'Text that appears when a mouse is hovered over the image',
+  // Remove this section if you don"t want any images
+  largeImageKey: "image-name-here",
+  largeImageText: "Text that appears when a mouse is hovered over the image",
 
   // If you would like a small icon-like image
-  // Remove this section if you don't want this
-  smallImageKey: 'image-name-here',
-  smallImageText: 'Text that appears when a mouse is hovered over the image',
+  // Remove this section if you don"t want this
+  smallImageKey: "image-name-here",
+  smallImageText: "Text that appears when a mouse is hovered over the image",
 
   // If you wish to add any links
   // Remove this if you do not want any links
   buttons: [
-    {label: 'The text on the button', url: 'The link'},
-    {label: 'The text on the button', url: 'The link'}
+    {label: "The text on the button", url: "The link"},
+    {label: "The text on the button", url: "The link"}
   ],
 
   // Do not remove
