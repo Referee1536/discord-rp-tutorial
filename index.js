8b63a37e2d911926b09238ad5451bf1d56e27f93
Referee1536 <66706835+Referee1536@users.noreply.github.com>
2021-02-26 11:57:50 -0500

    Updated grammar in comments

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ client.updatePresence({
   state: "Tertiary text",
 
   // If you added any images
-  // Remove this section if you don"t want any images
+  // Remove this section if you don't want any images
   largeImageKey: "image-name-here",
   largeImageText: "Text that appears when a mouse is hovered over the image",
 
   // If you would like a small icon-like image
-  // Remove this section if you don"t want this
+  // Remove this section if you don't want this
   smallImageKey: "image-name-here",
   smallImageText: "Text that appears when a mouse is hovered over the image",
 
